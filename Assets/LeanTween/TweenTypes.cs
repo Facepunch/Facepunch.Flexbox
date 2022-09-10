84512adc0b6bda99d9c4aaf42da307373e8827f1
Rohan Singh <rohan-singh@hotmail.com>
2022-09-10 01:33:17 -0400

    Example accordion-ish UI with collapsible sections that animate when opening/closing

---
+++ Assets/LeanTween/TweenTypes.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+
+public static class TweenMode
+{
+    public static AnimationCurve Punch = new AnimationCurve( new Keyframe(0.0f, 0.0f ), new Keyframe(0.112586f, 0.9976035f ), new Keyframe(0.3120486f, 0.01720615f ), new Keyframe(0.4316337f, 0.17030682f ), new Keyframe(0.5524869f, 0.03141804f ), new Keyframe(0.6549395f, 0.002909959f ), new Keyframe(0.770987f, 0.009817753f ), new Keyframe(0.8838775f, 0.001939224f ), new Keyframe(1.0f, 0.0f ) );
+}
---
+++ Assets/Scripts/AccordionToggle.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using UnityEngine;
+
+public class AccordionToggle : MonoBehaviour
+{
+    public enum AccordionAxis { Vertical, Horizontal }
+
+    public AccordionAxis Axis;
+    public FlexElement Element;
+    public CanvasGroup CanvasGroup;
+
+    [Header("Animation")]
+    [Min(0)]
+    public float Duration = 0.25f;
+    public LeanTweenType Ease = LeanTweenType.linear;
+
+    private static readonly  Action<float, object> DirtyLayoutForObject = (value, obj) =>
+    {
+        var self = (AccordionToggle)obj;
+        var element = self.Element;
+        if (element != null)
+        {
+            element.SetLayoutDirty();
+        }
+    };
+
+    public void Toggle()
+    {
+        SetExpanded(!IsExpanded);
+    }
+
+    public void SetExpanded(bool expanded)
+    {
+        if (Element == null)
+        {
+            return;
+        }
+
+        var isExpanded = IsExpanded;
+        if (expanded == isExpanded)
+        {
+            return;
+        }
+        
+        var targetGo = Element.gameObject;
+        var targetValue = isExpanded ? 0 : 1;
+
+        LeanTween.cancel(targetGo);
+        if (Axis == AccordionAxis.Vertical)
+        {
+            LeanTween.scaleY(targetGo, targetValue, Duration)
+                .setEase(Ease)
+                .setOnUpdate(DirtyLayoutForObject, this);
+        }
+        else
+        {
+            LeanTween.scaleX(targetGo, targetValue, Duration)
+                .setEase(Ease)
+                .setOnUpdate(DirtyLayoutForObject, this);
+        }
+
+        if (CanvasGroup != null)
+        {
+            var canvasGo = CanvasGroup.gameObject;
+            if (!ReferenceEquals(canvasGo, targetGo))
+            {
+                LeanTween.cancel(canvasGo);
+            }
+
+            LeanTween.alphaCanvas(CanvasGroup, targetValue, Duration).setEase(Ease);
+        }
+    }
+
+    public bool IsExpanded
+    {
+        get
+        {
+            if (Element == null)
+            {
+                return false;
+            }
+            
+            var rectTransform = (RectTransform)Element.transform;
+            var rtScale = rectTransform.localScale;
+            return Axis == AccordionAxis.Vertical
+                ? rtScale.y >= 1
+                : rtScale.x >= 1;
+        }
+    }
+}
