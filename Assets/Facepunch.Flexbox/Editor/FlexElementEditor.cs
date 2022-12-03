cce9f34aa150aac3b6d5b88ee409ddbb8442192d
Rohan Singh <rohan-singh@hotmail.com>
2022-12-02 22:51:53 -0500

    Split FlexElementEditor to have a shared base class too

--- Assets/Facepunch.Flexbox/Editor/FlexElementEditor.cs
+++ Assets/Facepunch.Flexbox/Editor/FlexElementEditor.cs
@@ -1,99 +1,35 @@
-﻿using Facepunch.Flexbox.Utility;
-using UnityEditor;
+﻿using UnityEditor;
 
 namespace Facepunch.Flexbox
 {
     [CustomEditor(typeof(FlexElement))]
     [CanEditMultipleObjects]
-    public class FlexElementEditor : Editor
+    public class FlexElementEditor : FlexElementEditorBase
     {
         private SerializedProperty _flexDirection;
         private SerializedProperty _justifyContent;
         private SerializedProperty _alignItems;
         private SerializedProperty _padding;
-        private SerializedProperty _basis;
         private SerializedProperty _gap;
-        private SerializedProperty _grow;
-        private SerializedProperty _shrink;
-        private SerializedProperty _alignSelf;
-        private SerializedProperty _isAbsolute;
-        private SerializedProperty _autoSizeX, _autoSizeY;
-        private SerializedProperty _minWidth, _maxWidth;
-        private SerializedProperty _minHeight, _maxHeight;
 
-        public void OnEnable()
+        public override void OnEnable()
         {
+            base.OnEnable();
+
             _flexDirection = serializedObject.FindProperty("FlexDirection");
             _justifyContent = serializedObject.FindProperty("JustifyContent");
             _alignItems = serializedObject.FindProperty("AlignItems");
             _padding = serializedObject.FindProperty("Padding");
-            _basis = serializedObject.FindProperty("Basis");
             _gap = serializedObject.FindProperty("Gap");
-            _grow = serializedObject.FindProperty("Grow");
-            _shrink = serializedObject.FindProperty("Shrink");
-            _alignSelf = serializedObject.FindProperty("AlignSelf");
-            _isAbsolute = serializedObject.FindProperty("IsAbsolute");
-            _autoSizeX = serializedObject.FindProperty("AutoSizeX");
-            _autoSizeY = serializedObject.FindProperty("AutoSizeY");
-            _minWidth = serializedObject.FindProperty("MinWidth");
-            _maxWidth = serializedObject.FindProperty("MaxWidth");
-            _minHeight = serializedObject.FindProperty("MinHeight");
-            _maxHeight = serializedObject.FindProperty("MaxHeight");
         }
 
-        public override void OnInspectorGUI()
+        protected override void LayoutSection()
         {
-            serializedObject.Update();
-
-            EditorGUILayout.LabelField("Layout", EditorStyles.boldLabel);
             EditorGUILayout.PropertyField(_flexDirection);
             EditorGUILayout.PropertyField(_justifyContent);
             EditorGUILayout.PropertyField(_alignItems);
             EditorGUILayout.PropertyField(_padding);
             EditorGUILayout.PropertyField(_gap);
-
-            EditorGUILayout.Space(5);
-            EditorGUILayout.LabelField("Behavior", EditorStyles.boldLabel);
-            EditorGUILayout.PropertyField(_isAbsolute);
-
-            if (!serializedObject.isEditingMultipleObjects && !_isAbsolute.boolValue)
-            {
-                var elem = (FlexElement)serializedObject.targetObject;
-                if (!FlexUtility.IsPrefabRoot(elem.gameObject))
-                {
-                    var parentObj = elem.transform.parent;
-                    if (parentObj == null || !parentObj.TryGetComponent<FlexElementBase>(out _))
-                    {
-                        EditorGUILayout.HelpBox("This element has no parent FlexElement. It should probably be marked as absolute.", MessageType.Warning);
-                    }
-                }
-                else
-                {
-                    EditorGUILayout.HelpBox("If this prefab will not spawn parented to another FlexElement then IsAbsolute should be enabled.", MessageType.Info);
-                }
-            }
-
-            if (!_isAbsolute.hasMultipleDifferentValues)
-            {
-                if (_isAbsolute.boolValue)
-                {
-                    EditorGUILayout.PropertyField(_autoSizeX);
-                    EditorGUILayout.PropertyField(_autoSizeY);
-                }
-                else
-                {
-                    EditorGUILayout.PropertyField(_basis);
-                    EditorGUILayout.PropertyField(_grow);
-                    EditorGUILayout.PropertyField(_shrink);
-                    EditorGUILayout.PropertyField(_alignSelf);
-                    EditorGUILayout.PropertyField(_minWidth);
-                    EditorGUILayout.PropertyField(_maxWidth);
-                    EditorGUILayout.PropertyField(_minHeight);
-                    EditorGUILayout.PropertyField(_maxHeight);
-                }
-            }
-
-            serializedObject.ApplyModifiedProperties();
         }
     }
 }
---
+++ Assets/Facepunch.Flexbox/Editor/FlexElementEditorBase.cs
@@ -0,0 +1,85 @@
+﻿using Facepunch.Flexbox.Utility;
+using UnityEditor;
+
+namespace Facepunch.Flexbox
+{
+    public abstract class FlexElementEditorBase : Editor
+    {
+        private SerializedProperty _basis;
+        private SerializedProperty _grow;
+        private SerializedProperty _shrink;
+        private SerializedProperty _alignSelf;
+        private SerializedProperty _isAbsolute;
+        private SerializedProperty _autoSizeX, _autoSizeY;
+        private SerializedProperty _minWidth, _maxWidth;
+        private SerializedProperty _minHeight, _maxHeight;
+
+        public virtual void OnEnable()
+        {
+            _basis = serializedObject.FindProperty("Basis");
+            _grow = serializedObject.FindProperty("Grow");
+            _shrink = serializedObject.FindProperty("Shrink");
+            _alignSelf = serializedObject.FindProperty("AlignSelf");
+            _isAbsolute = serializedObject.FindProperty("IsAbsolute");
+            _autoSizeX = serializedObject.FindProperty("AutoSizeX");
+            _autoSizeY = serializedObject.FindProperty("AutoSizeY");
+            _minWidth = serializedObject.FindProperty("MinWidth");
+            _maxWidth = serializedObject.FindProperty("MaxWidth");
+            _minHeight = serializedObject.FindProperty("MinHeight");
+            _maxHeight = serializedObject.FindProperty("MaxHeight");
+        }
+
+        protected abstract void LayoutSection();
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.LabelField("Layout", EditorStyles.boldLabel);
+            LayoutSection();
+
+            EditorGUILayout.Space(5);
+            EditorGUILayout.LabelField("Behavior", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(_isAbsolute);
+
+            if (!serializedObject.isEditingMultipleObjects && !_isAbsolute.boolValue)
+            {
+                var elem = (FlexElementBase)serializedObject.targetObject;
+                if (!FlexUtility.IsPrefabRoot(elem.gameObject))
+                {
+                    var parentObj = elem.transform.parent;
+                    if (parentObj == null || !parentObj.TryGetComponent<FlexElementBase>(out _))
+                    {
+                        EditorGUILayout.HelpBox("This element has no parent FlexElement. It should probably be marked as absolute.", MessageType.Warning);
+                    }
+                }
+                else
+                {
+                    EditorGUILayout.HelpBox("If this prefab will not spawn parented to another FlexElement then IsAbsolute should be enabled.", MessageType.Info);
+                }
+            }
+
+            if (!_isAbsolute.hasMultipleDifferentValues)
+            {
+                if (_isAbsolute.boolValue)
+                {
+                    EditorGUILayout.PropertyField(_autoSizeX);
+                    EditorGUILayout.PropertyField(_autoSizeY);
+                }
+                else
+                {
+                    EditorGUILayout.PropertyField(_basis);
+                    EditorGUILayout.PropertyField(_grow);
+                    EditorGUILayout.PropertyField(_shrink);
+                    EditorGUILayout.PropertyField(_alignSelf);
+                    EditorGUILayout.PropertyField(_minWidth);
+                    EditorGUILayout.PropertyField(_maxWidth);
+                    EditorGUILayout.PropertyField(_minHeight);
+                    EditorGUILayout.PropertyField(_maxHeight);
+                }
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
